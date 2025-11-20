4608450a6bbda817ff6d571762a1b4ea38832712
badgrammar <michael.erlangga2210@gmail.com>
2025-11-20 22:57:01 +0700

    chore: added helpdesk & backroom route group with middleware

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BkDashboardController;
+Use App\Http\Controllers\HdDashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::prefix('backroom')
+    ->as('backroom.')
+    ->middleware(['auth', 'role:backroom'])
+    ->group(function () {
+        Route::get('dashboard', [BkDashboardController::class, 'index']);
+    });
+
+Route::prefix('helpdesk')
+    ->as('helpdesk.')
+    ->middleware(['auth', 'role:helpdesk'])
+    ->group(function () {
+        Route::get('dashboard', [HdDashboardController::class, 'index']);
+    });
