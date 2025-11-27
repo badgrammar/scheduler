f1d98b804482e7a784ef69f61e50aff84e93c82a
badgrammar <michael.erlangga2210@gmail.com>
2025-11-27 21:59:04 +0700

    feat(ui): create topbar done

--- app/View/Components/Topbar.php
+++ app/View/Components/Topbar.php
@@ -5,6 +5,7 @@ namespace App\View\Components;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
+use Illuminate\Support\Facades\Auth;
 
 class Topbar extends Component
 {
@@ -23,6 +24,10 @@ class Topbar extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.topbar');
+        $user = Auth::user();
+
+        return view('components.topbar', [
+            'user' => $user
+        ]);
     }
 }
