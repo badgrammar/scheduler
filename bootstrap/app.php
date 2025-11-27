4647ba636ad12c9f3547b9f22a3ecc2a5204f14b
badgrammar <michael.erlangga2210@gmail.com>
2025-11-27 14:27:35 +0700

    bugfix: fixed authentication redirect behavior for guest and auth middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Session\Middleware\StartSession;
+use App\Http\Middleware\Authenticate;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,8 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
+            'guest' => App\Http\Middleware\RedirectAuthenticatedUsers::class,
             'role' => App\Http\Middleware\UserRole::class
         ]);
+
+        $middleware->redirectGuestsTo('/');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
