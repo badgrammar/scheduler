e17de1a1a0f5feb779d897392a7316d14a625806
badgrammar <michael.erlangga2210@gmail.com>
2025-11-27 17:18:21 +0700

    feat: create layout for the app

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Scheduler App</title>
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded" rel="stylesheet" />
     @vite('resources/css/app.css')
 </head>
 <body>
-    <div class="w-full flex mt-48 justify-center">
+    <div class="flex mt-48 justify-center">
         {{ $slot }}
     </div>
 </body>
