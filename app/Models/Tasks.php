85326f9de575d36d0e6ccb19e15c80520f1c7e86
badgrammar <michael.erlangga2210@gmail.com>
2025-11-27 23:47:02 +0700

    feat: create tasks model & form relationships

--- app/Models/Tasks.php
+++ app/Models/Tasks.php
@@ -4,19 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Logs extends Model
+class Tasks extends Model
 {
-    protected $table = 'logs';
+    protected $table = 'tasks';
 
     protected $guarded = [];
 
     public function user()
     {
         return $this->belongsTo(User::class);
-    }
-
-    public function task()
-    {
-        return $this->belongsTo(Tasks::class);
     }
 }
