2d1183a3bdbbd5ad4942a792321014964628d76d
badgrammar <michael.erlangga2210@gmail.com>
2025-11-27 18:01:40 +0700

    feat(ui): create sidebar

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,19 @@
+<div class="p-6 bg-white w-fit h-full border-right border border-gray-200">
+    <ul class="space-y-6">
+        <li>
+            <a href="{{ route('backroom.dashboard') }}">
+                <span class="material-symbols-rounded">dashboard</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <span class="material-symbols-rounded">calendar_month</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <span class="material-symbols-rounded">assignment</span>
+            </a>
+        </li>
+    </ul>
+</div>
