e85278877b973d6255365118b9d4adf98c401ec2
badgrammar <michael.erlangga2210@gmail.com>
2025-11-27 17:09:38 +0700

    chore: change authentication layout component naming

--- app/View/Components/authentication.php
+++ app/View/Components/authentication.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class authentication extends Component
+class Authentication extends Component
 {
     /**
      * Create a new component instance.
